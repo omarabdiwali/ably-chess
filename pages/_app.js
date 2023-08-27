ae09dc141f4ddfeaa60035793cd0825fc9cb9430
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-08-27 05:34:03 -0600

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Chess</title>
+        <meta name="google-site-verification" content="I8lDd4E2396ERE7e2VqqxppEraRzY_InsjQyuqZRsqI" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
