d73c682c04d53e4b6411401db5f82e70d26ffcfe
Omar Abdiwali <omarabdiwali17@gmail.com>
2025-08-06 07:26:36 -0600

    added tab icon

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>PlayChess</title>
+        {/* Favicon creted by https://www.flaticon.com/authors/bqlqn. */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="google-site-verification" content="AFBrxKgnj1l1SuZP7euHyl2zmEnTh5kGcTZgsZZ7bMc" />
       </Head>
       <AblyProvider>
